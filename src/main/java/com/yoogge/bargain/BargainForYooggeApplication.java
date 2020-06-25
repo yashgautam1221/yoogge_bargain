d56ad7d164f0ebf531dca592c61bacd574222be7
Sahib Saify <sahibsaify98@gmail.com>
2020-06-25 18:38:36 +0530

    initial commit

---
+++ src/main/java/com/yoogge/bargain/BargainForYooggeApplication.java
@@ -0,0 +1,13 @@
+package com.yoogge.bargain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BargainForYooggeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BargainForYooggeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoogge/bargain/BargainForYooggeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoogge.bargain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BargainForYooggeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
